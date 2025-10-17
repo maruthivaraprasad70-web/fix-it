3c9ce3dd9a0b5c4639451e4e42b7bbebc0106655
maruthivaraprasad70-web <maruthivaraprasad70@gmail.com>
2025-10-17 14:39:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+#simple calculator
+a = int(input("Enter the first number:" ))
+b = int(input("Enter the second number:" ))
+print("addition",a+b)
+print("subraction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
